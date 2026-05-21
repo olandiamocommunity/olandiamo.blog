4cb085ed790cf02bfcffed948966d581b8f4a0d4
olandiamocommunity <olandiamo.community@gmail.com>
2026-05-21 13:12:39 +0000

    Configure Astro for GitHub Pages

--- astro.config.ts
+++ astro.config.ts
@@ -18,7 +18,8 @@ import { transformerFileName } from "./src/utils/transformers/fileName";
 import config from "./astro-paper.config";
 
 export default defineConfig({
-  site: config.site.url,
+  site: "https://olandiamocommunity.github.io",
+base: "/olandiamo.blog",
   integrations: [
     mdx(),
     sitemap({
